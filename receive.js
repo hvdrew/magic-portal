0050f1aaf616131cef29bc35cb1752fa29dea5b4
Andrew <andrew@merison.io>
2020-01-09 16:32:31 -0800

    Works!

--- receive.js
+++ receive.js
@@ -13,26 +13,44 @@
  */
 
 const getBroadcastAddresses = require('./lib/broadcastAddress');
-const os = require('os');
 const dgram = require('dgram');
+const http = require('http');
 
 function receiveHandler(cliInstance) {
     const key = cliInstance.key;
     const broadcastAddresses = getBroadcastAddresses();
     const client = dgram.createSocket('udp4');
     
-    client.bind(8000, '255.255.255.255', () => {
+    client.bind(8000, undefined, () => {
         client.setBroadcast(true);
     });
+    
+    client.on('message', (msg, rinfo) => {
+        if (msg.toString() == 'Paired') {
+            console.log('Pairing complete, requesting file');
+            http.get(`http://${rinfo.address}:3000`, (response) => {
+                let data = '';
+                
+                response.on('data', (chunk) => {
+                    data += chunk;
+                });
+                
+                response.on('end', () => {
+                    console.log(data);
+                });
+            }).on('error', error => console.error(error));
+            
+        }
+    })
 
-    broadcastAddresses.forEach(addr => {
-        client.send(key, 41234, addr, (err) => {
+    //broadcastAddresses.forEach(addr => {
+        client.send(key, 41234, broadcastAddresses[0], (err) => {
             // TODO: We want to start a server once this is completed since the sending client will have our IP from here on.
             // Need to spin up a web server, set some handlers for file transfer.
             // client.close();
-            console.log('sent key to ', addr)
+            console.log('sent key to ', broadcastAddresses[0])
         });
-    })
+    //})
 }
 
 module.exports = receiveHandler;
--- send.js
+++ send.js
@@ -10,6 +10,18 @@
  */
 
 const dgram = require('dgram');
+const fs = require('fs');
+const httpServer = require('http').createServer();
+
+function fileTransferServerHandler(file) {
+    httpServer.on('request', (req, res) => {
+        console.log('received request!')
+        const sourceFile = fs.createReadStream(file);
+        sourceFile.pipe(res);
+    });
+    
+    httpServer.listen(3000, () => console.log('Listening on 3000 for partner machine to connect.'));
+}
 
 function sendHandler(cliInstance) {
     // Set the key for the transfer
@@ -26,9 +38,14 @@ function sendHandler(cliInstance) {
     server.on('message', (msg, rinfo) => {
         if (msg == cliInstance.key) console.log(`Server got correct key!`);
         console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-        
         // TODO: If the key matches, we need to start trying to connect to the address we now have
         // On a predetermined port. This should be broken out into it's own separate method.
+        
+        server.send('Paired', rinfo.port, rinfo.address, (err) => {
+            console.log('Paired with ' + rinfo.address);
+            server.close();
+        });
+        fileTransferServerHandler(cliInstance.file);
     });
     
     server.on('listening', () => {
